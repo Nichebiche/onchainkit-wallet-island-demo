cd6e9fd9f7167a0cfddd91ce360acbb95cfc5137
Brendan at Coinbase <brendan.forster@coinbase.com>
2025-01-15 11:48:49 -0800

    update providers

--- app/providers.tsx
+++ app/providers.tsx
@@ -1,21 +1,22 @@
-'use client';
+"use client";
 
-import { base } from 'wagmi/chains';
-import { OnchainKitProvider } from '@coinbase/onchainkit';
-import type { ReactNode } from 'react';
+import { base } from "wagmi/chains";
+import { OnchainKitProvider } from "@coinbase/onchainkit";
+import type { ReactNode } from "react";
 
 export function Providers(props: { children: ReactNode }) {
-  return (
-    <OnchainKitProvider
-      apiKey={process.env.NEXT_PUBLIC_ONCHAINKIT_API_KEY}
-          chain={base}
-          config={{ appearance: { 
-            mode: 'auto',
-        }
-      }}
-    >
-      {props.children}
-    </OnchainKitProvider>
-  );
+    return (
+        <OnchainKitProvider
+            apiKey={process.env.NEXT_PUBLIC_ONCHAINKIT_API_KEY}
+            projectId={process.env.NEXT_PUBLIC_ONCHAINKIT_PROJECT_ID}
+            chain={base}
+            config={{
+                appearance: {
+                    mode: "auto",
+                },
+            }}
+        >
+            {props.children}
+        </OnchainKitProvider>
+    );
 }
-
